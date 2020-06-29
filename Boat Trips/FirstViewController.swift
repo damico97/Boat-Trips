e3511956afe3eb3b7b7c0586106600ff225988a9
Joey Damico <damico31097@gmail.com>
2020-06-29 16:30:18 -0400

    Initial Commit

---
+++ Boat Trips/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Boat Trips
+//
+//  Created by Joey Damico on 6/29/20.
+//  Copyright © 2020 Joey Damico. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Boat Trips/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Boat Trips
+//
+//  Created by Joey Damico on 6/29/20.
+//  Copyright © 2020 Joey Damico. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
